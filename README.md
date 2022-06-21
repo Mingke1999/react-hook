ec532a30069304f8eb8592006cc633447ab3b47b
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-21 13:28:47 +1000

    count example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### React Hook state: use state without class after 16.8
+const [count,setCount] = useState(0)
+default format use hook
--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -2,14 +2,24 @@ import { useState } from "react"
 
 function Hooks(){
   const [message, setMessage] = useState("using hooks no state")
+  const [count,setCount] = useState(0)
 
   function clickHandle(){
     setMessage("new hook")
+  }
+  function addHandle(){
+    setCount(count+1)
+  }
+  function minHandle(){
+    setCount(count-1)
   }
     return (
       <div>
         <h1>{message}</h1>
         <button onClick={clickHandle}>change message</button>
+        <h1>count {count}</h1>
+        <button onClick={addHandle}>increase</button>
+        <button onClick={minHandle}>decrease</button>
       </div>
     )
 }
