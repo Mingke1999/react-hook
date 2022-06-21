64d280cb4ba4208740b450b3bb996149f966157f
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-21 13:47:51 +1000

    effect hook

--- src/HookEffect.jsx
+++ src/HookEffect.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 //rfc
 export default function HookEffect() {
+
+    const [count,setCount] = useState(0)
+    const [num,setNum] = useState(0)
+    //componentDidMount
+    useEffect(()=>{ 
+        //console.log("automatically execute")
+        document.title = `tilte=${count}`
+    })
+    /**
+     * []means listening to the state alteration, nothing then don't listen
+     */
+    useEffect(()=>{     //wont't be covered->task independent
+        console.log("automatically execute")
+    },[num]) //listen to num
+    //componentDidUpdate
+    function addHandle(){
+        setCount(count+1)
+      }
+    function numHandle(){
+        setNum(num+1)
+      }
   return (
     <div>
-      Effect Hook
+      <h1>Effect Hook</h1>
+      <button onClick={addHandle}>increase</button>
+      <p>num={num}</p>
+      <button onClick={numHandle}>num++</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import HookEffect from "./HookEffect";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
